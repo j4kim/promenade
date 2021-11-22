6010e7e12d359e9a01e98e826bfc0bc2cb1c8d2d
Joaquim Perez <jivkim@gmail.com>
2021-11-22 22:40:02 +0100

    first promenade svg background attempt

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
 </template>
 
 <style lang="scss">
+body, html {
+  margin: 0;
+  padding: 0;
+}
 #app {
+  height: 100vh;
+  height: -webkit-fill-available;
+  min-height: auto;
+  width: 5000px;
+  background-image: url(assets/promenade.svg);
 }
 </style>
