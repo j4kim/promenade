872f7ab9954b6e1c00a73df1775c412481f4a2d3
Joaquim Perez <jivkim@gmail.com>
2022-01-02 19:37:55 +0100

    add points

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template>
-  <div id="app">
+  <div id="app"
+    :style="{
+      width: `${width}px`
+    }"
+  >
+    <div
+      class="point"
+      v-for="pos in positions"
+      :key="pos.toString()"
+      :style="{
+        left: `${pos[0]}%`,
+        top: `${pos[1]}%`,
+      }"
+    >
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    width: 3000,
+    positions: [
+      [23, 61],
+      [45, 77],
+    ]
+  }),
+  mounted() {
+    this.resize()
+    window.onresize = this.resize
+  },
+  methods: {
+    resize() {
+      this.width = this.$el.offsetHeight * 3
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 body, html {
@@ -13,7 +48,17 @@ body, html {
   height: 100vh;
   max-height: -webkit-fill-available;
   min-height: auto;
-  width: 5000px;
   background-image: url(assets/promenade.svg);
+  position: relative;
+}
+.point {
+  $point-size: 3vh;
+  position: absolute;
+  background: rgb(223, 163, 195);
+  width: $point-size;
+  height: $point-size;
+  border-radius: $point-size / 2;
+  margin-left: -$point-size / 2;
+  margin-top: -$point-size / 2;
 }
 </style>
