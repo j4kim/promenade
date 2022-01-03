7730402cc57a3dabbd421177548ca4bd4821b44f
Joaquim Perez <jivkim@gmail.com>
2022-01-03 17:55:36 +0100

    open/close bubble

--- src/Bubble.vue
+++ src/Bubble.vue
@@ -8,20 +8,34 @@
   >
     <div class="point"></div>
     <div class="line"></div>
-    <div class="rect">
-      Summary here
+    <div
+      class="rect"
+      :class="opened ? 'opened' : ''"
+      @click="opened = !opened"
+    >
+      <div v-if="!opened" class="summary">
+        Summary here
+      </div>
+      <div v-else class="content">
+        Content here
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['x', 'y']
+  props: ['x', 'y'],
+
+  data: () => ({
+    opened: false
+  })
 }
 </script>
 
 <style lang="scss">
 .bubble {
+  cursor: pointer;
   position: absolute;
   .point, .line, .rect {
     position: absolute;
@@ -51,6 +65,16 @@ export default {
     border-radius: 16px;
     box-sizing: border-box;
     padding: 16px;
+    transition: width .3s, height .4s;
+    &.opened {
+      $width: min(300px, 90vw);
+      $height: min(400px, 90vh);
+      width: $width;
+      height: $height;
+    }
+    &:hover {
+      outline: 1px solid white;
+    }
   }
 }
 </style>
