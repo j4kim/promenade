c897a006252826c4059ffc4d57b72780c5be18e8
Joaquim Perez <jivkim@gmail.com>
2022-01-03 17:37:38 +0100

    base of bubble design

--- src/Bubble.vue
+++ src/Bubble.vue
@@ -1,10 +1,16 @@
 <template>
   <div
-    class="point"
+    class="bubble"
     :style="{
       left: `${x}%`,
       top: `${y}%`,
-    }">
+    }"
+  >
+    <div class="point"></div>
+    <div class="line"></div>
+    <div class="rect">
+      Summary here
+    </div>
   </div>
 </template>
 
@@ -15,14 +21,36 @@ export default {
 </script>
 
 <style lang="scss">
-.point {
-  $point-size: 3vh;
+.bubble {
   position: absolute;
-  background: white;
-  width: $point-size;
-  height: $point-size;
-  border-radius: $point-size / 2;
-  margin-left: -$point-size / 2;
-  margin-top: -$point-size / 2;
+  .point, .line, .rect {
+    position: absolute;
+    background: white;
+  }
+  .point {
+    $point-size: 3vh;
+    width: $point-size;
+    height: $point-size;
+    top: -$point-size / 2;
+    left: -$point-size / 2;
+    border-radius: $point-size / 2;
+  }
+  .line {
+    $line-width: 0.8vh;
+    width: 60px;
+    height: $line-width;
+    top: -$line-width / 2;
+  }
+  .rect {
+    $width: min(140px, 40vw);
+    $height: min(180px, 40vh);
+    width: $width;
+    height: $height;
+    bottom: -60px;
+    left: 60px;
+    border-radius: 16px;
+    box-sizing: border-box;
+    padding: 16px;
+  }
 }
 </style>
