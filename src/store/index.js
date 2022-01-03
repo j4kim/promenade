c50edb8469af90af07ccb84e3a7f7ec957e422b9
Joaquim Perez <jivkim@gmail.com>
2022-01-03 23:51:38 +0100

    fetch products from woocommerce api, keys are requested

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,38 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let username = process.env.VUE_APP_API_USERNAME
+let password = process.env.VUE_APP_API_PASSWORD
+
+let headers = new Headers({
+  Authorization: 'Basic ' + btoa(`${username}:${password}`)
+})
+
+function api(uri){
+  let base = 'https://shop.estellezamme.com/wp-json'
+  let url = `${base}/${uri}`
+  return fetch(url, { headers }).then(r => r.json())
+}
+
 export default new Vuex.Store({
   state: {
-    posts: []
+    posts: [],
+    products: [],
   },
   mutations: {
     setPosts(state, posts) {
       state.posts = posts
-    }
+    },
+    setProducts(state, products) {
+      state.products = products
+    },
   },
   actions: {
     fetchData({ commit }) {
-      fetch('https://shop.estellezamme.com/wp-json/wp/v2/posts?_embed')
-        .then(r => r.json())
+      api('wp/v2/posts?_embed')
         .then(posts => commit('setPosts', posts))
+      api('wc/v3/products')
+        .then(products => commit('setProducts', products))
     }
   }
 })
