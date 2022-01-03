20d826e5d748c32de0525284c747a1343ebe18c9
Joaquim Perez <jivkim@gmail.com>
2022-01-03 16:55:04 +0100

    a algorithm to compute width and place x number of items on the map

--- src/Promenade.vue
+++ src/Promenade.vue
@@ -2,11 +2,11 @@
   <main :style="{ width: `${width}px` }">
     <div
       class="point"
-      v-for="pos in positions"
-      :key="pos.toString()"
+      v-for="point in points"
+      :key="point.toString()"
       :style="{
-        left: `${pos[0]}%`,
-        top: `${pos[1]}%`,
+        left: `${point[0]/sections}%`,
+        top: `${point[1]}%`,
       }"
     >
     </div>
@@ -20,15 +20,41 @@ export default {
     positions: [
       [23, 61],
       [45, 77],
-    ]
+      [63, 41.5],
+      [85, 59],
+    ],
+    items: 5
   }),
+
+  computed: {
+    points() {
+      return [...Array(this.items)].map((_, i) => {
+        let len = this.positions.length
+        let slice = Math.floor(i / len)
+        let position = this.positions[i % len]
+        return [
+          position[0] + (slice * 100),
+          position[1]
+        ]
+      })
+    },
+    sections() {
+      // if there is 4 positions available, and 6 items, we'll have 1.5 sections
+      // the +1 is a margin
+      return (this.items + 1) / this.positions.length
+    }
+  },
+
   mounted() {
     this.resize()
     window.onresize = this.resize
   },
+
   methods: {
     resize() {
-      this.width = this.$el.offsetHeight * 3
+      // map image has a ratio of 3/1, this is fixed
+      let fullMapWidth = this.$el.offsetHeight * 3
+      this.width = fullMapWidth * this.sections
     }
   }
 }
@@ -37,6 +63,8 @@ export default {
 <style lang="scss">
 main {
   background-image: url(assets/promenade.svg);
+  background-repeat: repeat-x;
+  background-size: auto 100%;
   height: 100%;
   position: relative;
   .point {
